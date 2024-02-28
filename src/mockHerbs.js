f19ff1be598e48624d559902fcf6e90db1641f47
Sakaa4Short <klanyenek@gmail.com>
2024-02-28 11:37:21 -0800

    React Components Completed along with added folders for organization

---
+++ src/mockHerbs.js
@@ -0,0 +1,28 @@
+let mockHerbs = [
+  {
+    name: "Marigold",
+    scientific_name: "Calendula officinalis",
+    summary: "Marigold has exceptional healing powers along with a prowess for healing wounds.",
+    benefit: "Antiseptic, Muscle Pain, Wound Treatment for Eczema and Sunburns",
+    warning: "Fresh Marigold is likely unsafe as comsumption fresh can cause diarrhea and severe irritation of stomach, intestines, bladder, and kidneys.",
+    image:"https://cdn.britannica.com/76/118076-050-756CD4F5/French-marigold.jpg",
+  },
+  {
+    name: "Blue Lotus Root",
+    scientific_name: "Nymphaea caerulea",
+    summary: "Also known as the Sacred Blue Lily, it's benefits have laid claims for thousands of years as a sleep aid, aphrodisiac and much more while it boasts having psychoactive properties.",
+    benefit: "Improves Sleep, Eases Anxiety and Aphrodisiac",
+    warning: "Psychoactive properties should not be mixes with other substances",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Lotus_india_cropped.jpg/1200px-Lotus_india_cropped.jpg"
+  },
+  {
+    name: "Damiana",
+    scientific_name: "Turnera diffusa",
+    summary: "A small shrub with beautiful yellow flowers, its prides itself in having aphrodisiatic properties while being a mood evelator",
+    benefit: "Improves Mood, Aphrodisiac & Eases Stomach Problem",
+    warning: "High doses are not safe for Neurological Reasons",
+    image: "https://www.herbalhealinginc.com/wp-content/uploads/HERBS-083.jpg"
+  }
+]
+
+export default mockHerbs
