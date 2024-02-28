be9fd5760df373c50d0f27cc68ec0caa02cfe37b
Sakaa4Short <klanyenek@gmail.com>
2024-02-28 12:33:46 -0800

    Corrected Routes for proper rendering of App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import './App.css';
 import { Routes, Route } from "react-router-dom";
-import mockUsers from './mockUsers.js';
-import mockHerbs from './mockHerbs.js';
+import { BrowserRouter, Link, Outlet, useRoutes } from 'react-router-dom';
+import Header from "./component/Header";
+import Footer from "./component/Footer";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -12,44 +12,35 @@ import HerbShow from "./pages/HerbShow";
 import HerbEdit from "./pages/HerbEdit";
 import NotFound from "./pages/NotFound";
 import HerbNew from "./pages/HerbNew";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import ContactUs from "./pages/ContactUs";
 import AboutUs from "./pages/AboutUs";
+import mockUsers from './mockUsers.js';
+import mockHerbs from './mockHerbs.js';
+import './App.css';
+
 
 const App = () => {
+
   (
-  <>
+    <>
       <h3>The Leaf Library</h3>    
-      <Header currentUser={currentUser} />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route
-          path="/herbsindex"
-          element={<HerbsIndex herbs={herbs} />}
-        />
-        <Route path="/myherbs" element={<HerbsProtectedIndex />} />
-        <Route
-          path="/herbsshow/:id"
-          element={<HerbShow herbs={herbs} />}
-        />
-        <Route
-          path="/herbsnew"
-          element={<HerbNew currentUser={currentUser} />}
-        />
-        <Route
-          path="/herbedit/:id"
-          element={
-            <HerbEdit currentUser={currentUser} herbs={herbs} />
-          }
-        />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/herbsindex" element={<HerbIndex />} />
+        <Route path="/myherbs" element={<HerbProtectedIndex />} />
+        <Route path="/herbshow/:id" element={<HerbShow />} />
+        <Route path="/herbnew" element={<HerbNew />} />
+        <Route path="/herbedit/:id" element={ <HerbEdit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
