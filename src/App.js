02536cbdb1643a197e248006e11a1866cdf8aef1
Sakaa4Short <klanyenek@gmail.com>
2024-02-28 11:52:52 -0800

    Added Routing to Pages and imports to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import mockUsers from './mockUsers.js';
+import mockHerbs from './mockHerbs.js';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import HerbIndex from "./pages/HerbIndex";
+import HerbProtectedIndex from "./pages/HerbProtectedIndex";
+import HerbShow from "./pages/HerbShow";
+import HerbEdit from "./pages/HerbEdit";
+import NotFound from "./pages/NotFound";
+import HerbNew from "./pages/HerbNew";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import ContactUs from "./pages/ContactUs";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
---
+++ src/pages/HerbEdit.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const HerbEdit = () => {
+  return (
+    <>
+
+    </>
+  )
+} 
+
+export default HerbEdit
