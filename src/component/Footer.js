fad0d335675a8be70296c5d50e334a5ce5d16f84
chantelcarter <chantel.alexia@gmail.com>
2024-03-07 14:54:35 -0800

    footer, index, and forms styling

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -6,17 +6,13 @@ const Footer = () => {
     return (
         <>
            <footer>
-            &copy; 2024 LEARN Double Stuffed Oreos | Chantel, Michael & Sakaa
-            <NavItem>
-            <NavLink to="/aboutus">
+            &copy; 2024 LEARN Double Stuffed Oreos | Chantel, Michael & Sakaa |
+            <NavLink to="/aboutus" className="footer-link">
                 About Us
-            </NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink to="/contactus">
+            </NavLink>|
+            <NavLink to="/contactus" className="footer-link">
                 Contact Us
             </NavLink>
-            </NavItem>
            </footer>
            
         </>
